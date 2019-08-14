cdb473f518487fe4cc8d95c79c05a3575d0d2382
deadpool <deadpool@KevinFlynn.mshome.net>
2019-08-14 15:48:21 -0500

    Initial Commit

---
+++ BasicTextOutput/src/jansproj/basic/BasicTextOne.java
@@ -0,0 +1,16 @@
+package jansproj.basic;
+
+public class BasicTextOne 
+{
+
+	public BasicTextOne() 
+	{
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Basic Text Line 01");
+		System.out.println("Basic Text Line 02");
+	}
+
+}
